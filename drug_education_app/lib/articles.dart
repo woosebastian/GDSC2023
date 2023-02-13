7e56097c6a3ad3fe8f18d4c53979dab0b654cda3
Sophie Han <32656696+sophiiehan@users.noreply.github.com>
2023-02-12 23:34:27 -0800

    added search barrr

--- drug_education_app/lib/articles.dart
+++ drug_education_app/lib/articles.dart
@@ -10,6 +10,35 @@ class Articles extends StatefulWidget {
 class _ArticlesState extends State<Articles> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: MyCustomForm(),
+    );
+  }
+}
+
+class MyCustomForm extends StatelessWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+            child: TextField(
+              cursorColor: Color.fromARGB(255, 150, 173, 227),
+              decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 174, 221, 227), width: 2)),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 150, 173, 227), width: 3)),
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a search term'),
+            ))
+      ],
+    );
   }
 }
