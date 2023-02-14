fa7cfdf00e820283a7dab42612b2e27c2837f1a6
Sebastian W <woosebastian@gmail.com>
2023-02-14 15:08:59 -0800

    change map page depending on orientation

--- drug_education_app/lib/maps.dart
+++ drug_education_app/lib/maps.dart
@@ -10,34 +10,60 @@ class Maps extends StatefulWidget {
 class _MapsState extends State<Maps> {
   @override
   Widget build(BuildContext context) {
-    //responsive widgets: https://codelabs.developers.google.com/codelabs/flutter-codelab-first#6, https://github.com/flutter/codelabs/blob/main/namer/step_08/lib/main.dart, and https://docs.flutter.dev/cookbook/design/orientation
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: OrientationBuilder(builder: (context, orientation) {
-        return GridView.count(
-            crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
-            children: [
-              //using Align to resize Containers: https://stackoverflow.com/questions/54717748/why-flutter-container-does-not-respects-its-width-and-height-constraints-when-it
-              SafeArea(
-                  child: Align(
-                      child: Container(
-                          // constraints: BoxConstraints.expand({300, 300}),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey,
-                          ),
-                          margin: const EdgeInsets.fromLTRB(10, 10, 10, 10)))),
-              Expanded(
-                  child: Align(
-                      child: Container(
-                          // constraints: BoxConstraints.expand(300, 100),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            color: Colors.grey,
-                          ),
-                          margin: const EdgeInsets.fromLTRB(10, 10, 10, 10)))),
-            ]);
-      }),
-    );
+    final deviceOrientation = MediaQuery.of(context).orientation;
+
+    return Scaffold(
+        body: Center(
+            child: deviceOrientation == Orientation.portrait
+                ? const MapsPortrait()
+                : const MapsLandscape()));
+  }
+}
+
+class MapsPortrait extends StatelessWidget {
+  const MapsPortrait({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+          height: 450,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey,
+          ),
+          margin: const EdgeInsets.fromLTRB(20, 20, 20, 5)),
+      Container(
+          height: 210,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey,
+          ),
+          margin: const EdgeInsets.fromLTRB(20, 20, 20, 5)),
+    ]);
+  }
+}
+
+class MapsLandscape extends StatelessWidget {
+  const MapsLandscape({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Container(
+          width: 525,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey,
+          ),
+          margin: const EdgeInsets.fromLTRB(60, 20, 0, 20)),
+      Container(
+          width: 265,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey,
+          ),
+          margin: const EdgeInsets.fromLTRB(20, 20, 5, 20)),
+    ]);
   }
 }
